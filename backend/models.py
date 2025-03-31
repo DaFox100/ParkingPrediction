61c176aec4b65fcac4eca622f2902d47308939af
Dmitriy Gamolya <109139706+DrowsyTM@users.noreply.github.com>
2025-03-31 02:08:10 -0700

    Import DATETIME instead of datetime for some reason is necessary

--- backend/models.py
+++ backend/models.py
@@ -1,23 +1,22 @@
-from pydantic import BaseModel as PydanticBaseModel
-from sqlalchemy import create_engine, Column, Datetime, Float
+from sqlalchemy import create_engine, Column, DATETIME, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from datetime import datetime
 from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).parent.parent  # Go to backend then to root
 DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'data', 'log.db')}"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL) # This opens the connection
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Interaction
 Base = declarative_base()
 
+# This "Datapoints" class utilizes ORM (mapping the SQL database to python object)
 class Datapoints(Base):
     __tablename__ = "datapoints"
 
-    timestamp = Column(Datetime, primary_key=True)
+    timestamp = Column(DATETIME, primary_key=True)
     South_status = Column(Float)
     South_Traffic_density = Column(Float)
     West_status = Column(Float)
