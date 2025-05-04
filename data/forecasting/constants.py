a6cac62181f0921d4cb374678c585491d176141c
John <john.gibbard@sjsu.edu>
2025-05-04 14:25:26 -0700

    Added event data to predictor model, as well as fixing a problem with the time encoding. Refactored some of the code to remove repeated code.

--- data/forecasting/constants.py
+++ data/forecasting/constants.py
@@ -7,7 +7,7 @@ DATA_DIR = Path(__file__).resolve().parent.parent # -> Forecasting -> data
 # ── DIRECTORIES ───────────────────────────────────────────────────────────────────
 LOGS_DIRECTORY = DATA_DIR / "records"
 MODEL_DIRECTORY = DATA_DIR / "forecasting" / "keras_models"
-
+EVENTS_DIRECTORY = DATA_DIR / "events"
 
 # ── CONSTANTS ───────────────────────────────────────────────────────────────────
 GARAGE_NAMES = ["south", "west", "north", "south_campus"]
@@ -16,9 +16,13 @@ GARAGE_NAMES = ["south", "west", "north", "south_campus"]
 # ── TRAINING CONSTANTS ───────────────────────────────────────────────────────────
 # Define parameters for long model
 LONG_SEQ = 8
-LONG_FUTURE_STEPS = 128
+LONG_FUTURE_STEPS = 196
 
 # Define parameters for short model
 SHORT_SEQ = 16
 SHORT_FUTURE_STEPS = 16
 
+ENABLE_TIME_ENCODING: bool          = True
+ENABLE_INSTR_DAY: bool              = True
+ENABLE_INSTR_NEXT_DAY: bool         = True
+ENABLE_EVENT_ENCODING: bool         = True
